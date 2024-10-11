01487066acb7a4843a306f8a24cf5aec35547d2b
kaimex9 <kaimexalabau@gmail.com>
2024-10-11 19:15:19 +0200

    nig

--- src/ComBank/Bank/BankAccount.php
+++ src/ComBank/Bank/BankAccount.php
@@ -1,4 +1,5 @@
-<?php namespace ComBank\Bank;
+<?php
+namespace ComBank\Bank;
 
 /**
  * Created by VS Code.
@@ -20,4 +21,43 @@ use ComBank\Transactions\Contracts\BankTransactionInterface;
 
 class BankAccount
 {
-    }
+private $balance;
+private $status;
+private $overdraft;
+
+public function getBalance()
+{
+return $this->balance;
+}
+
+public function setBalance($balance)
+{
+$this->balance = $balance;
+
+return $this;
+}
+
+public function getStatus()
+{
+return $this->status;
+}
+
+public function setStatus($status)
+{
+$this->status = $status;
+
+return $this;
+}
+
+public function getOverdraft()
+{
+return $this->overdraft;
+}
+
+public function setOverdraft($overdraft)
+{
+$this->overdraft = $overdraft;
+
+return $this;
+}
+}
